43f2a229f4fabf253ca55f33bcbbb5a15e9ef3ac
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-18 14:33:45 +0200

    added utility functions

--- Program.cs
+++ Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //FUNZIONI DI UTILITY
+
+            void StampaArray(int[] array)
+            {
+                Console.Write("(");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+                        Console.Write(", ");
+                }
+                Console.Write(")");
+            }
+
+            int Quadrato(int numero)
+            {
+                int newNumber = numero * numero;
+                return newNumber;
+                
+            }
+
+            int[] ElevaArrayAlQuadrato(int[] array)
+            {
+                int[] newArray = new int[array.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    newArray[i] = array[i] * array[i];
+                }
+                return newArray;
+            }
+
+            int sommaElementiArray(int[] array)
+            {
+                int sum = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i];
+                }
+                return sum;
+            }
         }
     }
 }
