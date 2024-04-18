e1654391e8760875743f71a839ce60b41a6c76b4
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-18 15:05:21 +0200

    bonus exercise finished

--- Program.cs
+++ Program.cs
@@ -45,12 +45,31 @@
                 return sum;
             }
 
-            int[] myArray = { 2, 6, 7, 5, 3, 9 };
-            StampaArray(myArray);
-            StampaArray(ElevaArrayAlQuadrato(myArray));
-            StampaArray(myArray);
-            Console.WriteLine($"La somma di tutti i numeri del primo array è: {sommaElementiArray(myArray)}");
-            Console.WriteLine($"La somma di tutti i numeri elevati al quadrato è: {sommaElementiArray(ElevaArrayAlQuadrato(myArray))}");
+            // BASIC PROGRAM
+            {
+                int[] myArray = { 2, 6, 7, 5, 3, 9 };
+                StampaArray(myArray);
+                StampaArray(ElevaArrayAlQuadrato(myArray));
+                StampaArray(myArray);
+                Console.WriteLine($"La somma di tutti i numeri del primo array è: {sommaElementiArray(myArray)}");
+                Console.WriteLine($"La somma di tutti i numeri elevati al quadrato è: {sommaElementiArray(ElevaArrayAlQuadrato(myArray))}");
+            }
+            // BONUS PROGRAM WHERE USER CAN CHOOSE THE NUMBERS HE WANTS TO USE
+            {
+                Console.WriteLine("Scegli quanti numeri vuoi utilizzare:");
+                int numbers = Convert.ToInt32(Console.ReadLine());
+                int[] userArray = new int[numbers];
+                for (int i = 0; i < userArray.Length; i++)
+                {
+                    Console.WriteLine($"Inserisci il numero {i + 1}/{numbers}:");
+                    userArray[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                StampaArray(userArray);
+                StampaArray(ElevaArrayAlQuadrato(userArray));
+                StampaArray(userArray);
+                Console.WriteLine($"La somma di tutti i numeri del primo array è: {sommaElementiArray(userArray)}");
+                Console.WriteLine($"La somma di tutti i numeri elevati al quadrato è: {sommaElementiArray(ElevaArrayAlQuadrato(userArray))}");
+            }
         }
     }
 }
